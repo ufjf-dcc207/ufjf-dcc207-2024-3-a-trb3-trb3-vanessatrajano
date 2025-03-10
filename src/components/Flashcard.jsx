2dcf4aa377bed39fddcb8d3d841890f838b6c976
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-03-10 19:53:37 -0300

    Cria estrutura do componente Flashcard

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,11 +1,22 @@
-import './Flashcard.css'
+import "./Flashcard.css";
 
 function Flashcard() {
   return (
     <>
-     Flashcard funciona!
+      <h2>palavra</h2>
+      <div>
+        <p>
+          <strong>Definição:</strong> xxxxxxxxxx
+        </p>
+        <p>
+          <strong>Exemplo:</strong> xxxxxxxxxxx
+        </p>
+        <p>
+          <strong>Pronúncia:</strong> xxxxxxxxx
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default Flashcard
+export default Flashcard;
