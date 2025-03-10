98c4f587f5bf518b4d5a2ed9c10a8a83271f1241
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-03-10 20:22:20 -0300

    Adiciona estrutura da página

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,21 @@ function App() {
   return (
     <>
       <h1>English Flash Cards</h1>
+      <div>
+        <input type="text" placeholder="Digite uma palavra em inglês" />
+        <button>Adicionar Palavra</button>
+      </div>
       <Flashcard
         word="aaaaaa"
         definition="aaaaaaaa"
         example="aaaaaaaaaaa"
         pronunciation="aaaaaaaaa"
       />
+
+      <div>
+        <button>Flashcard Anterior</button>
+        <button>Próximo Flashcard</button>
+      </div>
     </>
   );
 }
