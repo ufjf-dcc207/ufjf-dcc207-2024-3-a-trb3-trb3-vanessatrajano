82d4ae0f5f8630cbe7c3fe924358e15a377c4201
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-03-14 16:24:57 -0300

    Cria lógica para adicionar palavra com o enter

--- src/components/WordInput.jsx
+++ src/components/WordInput.jsx
@@ -1,12 +1,19 @@
 import "./WordInput.css";
 
 function WordInput({ referencia, onClique }) {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      onClique();
+    }
+  };
+
   return (
     <div className="input-container">
       <input
         type="text"
         placeholder="Type a word in english"
         ref={referencia}
+        onKeyDown={handleKeyDown}
       />
       <button onClick={onClique}>Add word</button>
     </div>
